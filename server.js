757dcb4173b1e5c804d6581b8f3f6ff9b6f09a57
patricksebastianm-netizen <patricksebastianm@gmail.com>
2026-04-07 16:08:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require("express");
-const app = express();
+const { Pool } = require("pg");
 
+const app = express();
 app.use(express.json());
 
+// Verbindung zur Datenbank
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// Test Route
 app.get("/", (req, res) => {
-    res.send("Backend läuft 🚀");
+  res.send("Backend läuft 🚀");
+});
+
+// Test Datenbank
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("DB Fehler");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server läuft auf Port " + PORT);
+  console.log("Server läuft auf Port " + PORT);
 });
